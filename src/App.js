03c090e18ae3ef3a742616a52a2f76bfd4ee40bf
Kritika Sharma <kritikasharma1996@rediff.com>
2018-12-30 20:03:12 -0500

    scrollbar added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+import Scroll from './Scroll';
 import './App.css';
 
+//this is a smart component as it has the class syntax and it has a state
 class App extends Component {
 	constructor(){
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}
+	}
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => this.setState({ robots: users }));
 	}
 
 	onSearchChange = (event) => {
@@ -21,13 +28,20 @@ class App extends Component {
 		const filteredRobots = this.state.robots.filter(robot => {
 			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		})
-		return(
-			<div className = 'tc'>	
-				<h1 className='f1'>RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
-				<CardList robots={filteredRobots}/>
-			</div>	
-		);
+		if(this.state.robots.length===0){
+			return <h1>Loading</h1>
+		}
+		else{
+			return(
+				<div className = 'tc'>	
+					<h1 className='f1'>RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange}/>
+					<Scroll>
+						<CardList robots={filteredRobots}/>
+					</Scroll>					
+				</div>	
+			);
+		}
 	}
 }
 
