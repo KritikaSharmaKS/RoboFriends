3201bfce99475822f8305865c1035be3c307b825
Kritika Sharma <kritikasharma1996@rediff.com>
2018-12-30 21:07:32 -0500

    Scroll Bar styling imporved

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
 	return (
-		<div style = {{ overflowY: 'scroll', border: '5px solid black', height: '500px' }}>
+		<div style = {{ overflowY: 'scroll', border: '5px solid white', margin: '6px', height: '300px' }}>
 			{props.children}
 		</div>
 
